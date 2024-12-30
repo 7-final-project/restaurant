54e5f1ad831ac203d39823d32b6f569f28510358
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-30 17:44:33 +0900

    [ fix ] 수용 테이블 수 검증 어노테이션 수정 ( #1 )

--- src/main/java/com/qring/restaurant/presentation/v1/req/PostRestaurantReqDTOV1.java
+++ src/main/java/com/qring/restaurant/presentation/v1/req/PostRestaurantReqDTOV1.java
@@ -3,6 +3,7 @@ package com.qring.restaurant.presentation.v1.req;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -33,7 +34,7 @@ public class PostRestaurantReqDTOV1 {
         @NotBlank(message = "전화번호를 입력해주세요")
         private String tel;
 
-        @NotNull(message = "수용 테이블 수를 입력해주세요")
+        @Positive(message = "수용 테이블 수를 입력해주세요")
         private int capacity;
 
         @NotBlank(message = "주소를 입력해주세요")
