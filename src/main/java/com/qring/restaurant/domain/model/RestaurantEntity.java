f2151a9ef7b97a354efc720a3da49c9c4642730c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-30 14:39:39 +0900

    [ feat ] 생성자, 수정자 nullable = false 제약조건 추가 ( #1 )

--- src/main/java/com/qring/restaurant/domain/model/RestaurantEntity.java
+++ src/main/java/com/qring/restaurant/domain/model/RestaurantEntity.java
@@ -61,10 +61,10 @@ public class RestaurantEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    @Column(name = "created_by", updatable = false)
+    @Column(name = "created_by", nullable = false, updatable = false)
     private String createdBy;
 
-    @Column(name = "modified_by")
+    @Column(name = "modified_by", nullable = false)
     private String modifiedBy;
 
     @Column(name = "deleted_by")
