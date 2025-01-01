b3b8143cb7e5b80d55f1949d995ad957737ac57d
Jun <dudwns1852@naver.com>
2025-01-02 00:01:41 +0900

    [ feat ] 카테고리 검색 API 개발 ( #3 )

--- src/main/java/com/qring/restaurant/presentation/v1/controller/CategoryControllerV1.java
+++ src/main/java/com/qring/restaurant/presentation/v1/controller/CategoryControllerV1.java
@@ -2,14 +2,21 @@ package com.qring.restaurant.presentation.v1.controller;
 
 import com.qring.restaurant.application.global.dto.ResDTO;
 import com.qring.restaurant.application.v1.res.CategoryPostResDTOV1;
+import com.qring.restaurant.application.v1.res.CategorySearchResDTOV1;
 import com.qring.restaurant.domain.model.CategoryEntity;
 import com.qring.restaurant.infrastructure.docs.CategoryControllerSwagger;
 import com.qring.restaurant.presentation.v1.req.PostCategoryReqDTOV1;
 import com.qring.restaurant.presentation.v1.req.PutCategoryDTOV1;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/restaurants/category")
@@ -32,6 +39,36 @@ public class CategoryControllerV1 implements CategoryControllerSwagger {
                         .data(CategoryPostResDTOV1.of(dummyReviewEntity))
                         .build(),
                 HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<ResDTO<CategorySearchResDTOV1>> searchBy(@PageableDefault(sort = "id") Pageable pageable,
+                                                                   @RequestParam(name = "name", required = false) String name) {
+
+        // -- TODO : 더미 데이터를 사용한 카테고리 검색. 추후 실제 데이터로 변경
+        List<CategoryEntity> dummyCategories = List.of(
+                CategoryEntity.builder()
+                        .name("한식")
+                        .build(),
+                CategoryEntity.builder()
+                        .name("양식")
+                        .build(),
+                CategoryEntity.builder()
+                        .name("일식")
+                        .build()
+        );
+
+        Page<CategoryEntity> dummyPage = new PageImpl<>(dummyCategories, pageable, dummyCategories.size());
+
+        // -- 응답 DTO로 변환
+        return new ResponseEntity<>(
+                ResDTO.<CategorySearchResDTOV1>builder()
+                        .code(HttpStatus.OK.value())
+                        .message("카테고리 검색에 성공했습니다.")
+                        .data(CategorySearchResDTOV1.of(dummyPage))
+                        .build(),
+                HttpStatus.OK
         );
     }
 
