c05651d0fbfab28d10a736d1496d3566c4de25b8
Jun <dudwns1852@naver.com>
2025-01-01 23:32:09 +0900

    [ feat ] 카테고리 수정 API 수정 ( #3 )

--- src/main/java/com/qring/restaurant/presentation/v1/controller/CategoryControllerV1.java
+++ src/main/java/com/qring/restaurant/presentation/v1/controller/CategoryControllerV1.java
@@ -36,21 +36,15 @@ public class CategoryControllerV1 implements CategoryControllerSwagger {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ResDTO<CategoryPostResDTOV1>> putBy(@RequestHeader("X-User-Id") Long userId,
+    public ResponseEntity<ResDTO<Object>> putBy(@RequestHeader("X-User-Id") Long userId,
                                                                @PathVariable Long id,
                                                                @Valid @RequestBody PutCategoryDTOV1 dto) {
-        // -----
-        // TODO : 더미데이터입니다.
-        CategoryEntity dummyReviewEntity = CategoryEntity.builder()
-                .name("수정 한식")
-                .build();
-        // ----- 추후 삭제
+
 
         return new ResponseEntity<>(
-                ResDTO.<CategoryPostResDTOV1>builder()
+                ResDTO.builder()
                         .code(HttpStatus.OK.value())
                         .message("카테고리 수정에 성공했습니다.")
-                        .data(CategoryPostResDTOV1.of(dummyReviewEntity))
                         .build(),
                 HttpStatus.OK
         );
