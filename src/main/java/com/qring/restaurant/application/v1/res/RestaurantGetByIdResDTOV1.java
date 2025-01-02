cd9126067aeb1345b89c87a87906a103795cfc37
Jun <dudwns1852@naver.com>
2025-01-02 16:41:45 +0900

    [ feat ] 식당 상세 조회 응답 총 평균 평점 추가  ( #3 )

--- src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
+++ src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
@@ -36,6 +36,7 @@ public class RestaurantGetByIdResDTOV1 {
         private String tel;
         private String address;
         private String addressDetails;
+        private Double ratingAverage
         private List<OperatingHour> operatingHourList;
 
         public static Restaurant from(RestaurantEntity restaurantEntity) {
@@ -46,6 +47,7 @@ public class RestaurantGetByIdResDTOV1 {
                     .tel(restaurantEntity.getTel())
                     .address(restaurantEntity.getAddress())
                     .addressDetails(restaurantEntity.getAddressDetails())
+                    .ratingAverage(restaurantEntity.getRatingAverage())
                     .operatingHourList(OperatingHour.from(restaurantEntity.getOperatingHourEntityList()))
                     .build();
         }
