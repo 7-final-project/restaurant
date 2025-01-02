65653fc78f8eaee92a4ab16b88db50bf089d3f75
Jun <dudwns1852@naver.com>
2025-01-02 17:55:49 +0900

    [ feat ] 식당 상세 조회 응답 운영 상태 추가 ( #3 )

--- src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
+++ src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
@@ -2,6 +2,7 @@ package com.qring.restaurant.application.v1.res;
 
 import com.qring.restaurant.domain.model.OperatingHourEntity;
 import com.qring.restaurant.domain.model.RestaurantEntity;
+import com.qring.restaurant.domain.model.constraint.OperationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -37,6 +38,7 @@ public class RestaurantGetByIdResDTOV1 {
         private String address;
         private String addressDetails;
         private Double ratingAverage;
+        private OperationStatus operationStatus;
         private List<OperatingHour> operatingHourList;
 
         public static Restaurant from(RestaurantEntity restaurantEntity) {
@@ -48,6 +50,7 @@ public class RestaurantGetByIdResDTOV1 {
                     .address(restaurantEntity.getAddress())
                     .addressDetails(restaurantEntity.getAddressDetails())
                     .ratingAverage(restaurantEntity.getRatingAverage())
+                    .operationStatus(restaurantEntity.getOperationStatus())
                     .operatingHourList(OperatingHour.from(restaurantEntity.getOperatingHourEntityList()))
                     .build();
         }
