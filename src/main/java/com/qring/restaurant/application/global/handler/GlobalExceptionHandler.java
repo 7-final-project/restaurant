e39e8c8719cf2644b45184187241835530fe617c
Jun <dudwns1852@naver.com>
2025-01-03 16:04:36 +0900

    [ feat ] 글로벌 익셉션 핸들러를 Swagger 문서화에서 제외 ( #3 )

--- src/main/java/com/qring/restaurant/application/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/qring/restaurant/application/global/handler/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.qring.restaurant.application.global.handler;
 
 import com.qring.restaurant.application.global.dto.ResDTO;
 import com.qring.restaurant.application.global.exception.RestaurantException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
