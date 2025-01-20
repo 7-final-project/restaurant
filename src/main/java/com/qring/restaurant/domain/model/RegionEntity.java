066839d61ff653ab78929f1a7a91dd3822d01fbf
Yang ye lin <oky07031217@gmail.com>
2025-01-20 16:36:45 +0900

    [ refactor ] 지역 엔티티 수정 ( #18 )

--- src/main/java/com/qring/restaurant/domain/model/RegionEntity.java
+++ src/main/java/com/qring/restaurant/domain/model/RegionEntity.java
@@ -7,8 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -38,10 +36,6 @@ public class RegionEntity {
     @JoinColumn(name = "parent_id")
     private RegionEntity parent;
 
-    // 자식 지역 리스트
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RegionEntity> children = new ArrayList<>();
-
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -62,37 +56,32 @@ public class RegionEntity {
     @Column(name = "deleted_by")
     private String deletedBy;
 
-    public void setParent(RegionEntity parent) {
-        this.parent = parent;
-    }
-
-    // 최상위 지역 생성자
-    @Builder
-    public RegionEntity(String code, String name, String createdBy) {
-        this.code = code;
-        this.name = name;
-        this.depth = 0; // 최상위 지역의 depth는 0
-        this.createdBy = createdBy;
-        this.modifiedBy = createdBy;
-    }
-
-    // 하위 지역 생성자
+    // 지역 생성자
     @Builder
     public RegionEntity(String code, String name, RegionEntity parent, String createdBy) {
         this.code = code;
         this.name = name;
         this.parent = parent;
-        this.depth = 0;
-        //this.depth = parent.getDepth() + 1; // 부모의 depth + 1
+        this.depth = (parent != null) ? parent.getDepth() + 1 : 0;
         this.createdBy = createdBy;
         this.modifiedBy = createdBy;
     }
 
-    // 지역 생성 메서드
+    // 부모 지역 생성 메서드
     public static RegionEntity createRegionEntity(String code, String name, String username) {
         return RegionEntity.builder()
                 .code(code)
                 .name(name)
+                .createdBy(username)
+                .build();
+    }
+
+    // 자식 지역 생성 메서드
+    public static RegionEntity createRegionEntity(String code, String name,RegionEntity parentEntity, String username) {
+        return RegionEntity.builder()
+                .code(code)
+                .name(name)
+                .parent(parentEntity)
                 .createdBy(username)
                 .build();
     }
--- src/main/java/com/qring/restaurant/infrastructure/InitialRegionDataInserter.java
+++ src/main/java/com/qring/restaurant/infrastructure/InitialRegionDataInserter.java
@@ -23,72 +23,83 @@ public class InitialRegionDataInserter implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
 
-        // 초기 지역 데이터 생성
-        List<RegionEntity> initialRegions = List.of(
-                new RegionEntity("11", "서울특별시", null, "admin"),
-                new RegionEntity("21", "부산광역시", null, "admin"),
-                new RegionEntity("22", "대구광역시", null, "admin"),
-                new RegionEntity("23", "인천광역시", null, "admin"),
-                new RegionEntity("24", "광주광역시", null, "admin"),
-                new RegionEntity("25", "대전광역시", null, "admin"),
-                new RegionEntity("26", "울산광역시", null, "admin"),
-                new RegionEntity("29", "세종특별자치시", null, "admin"),
-                new RegionEntity("31", "경기도", null, "admin"),
-                new RegionEntity("32", "강원특별자치도", null, "admin"),
-                new RegionEntity("33", "충청북도", null, "admin"),
-                new RegionEntity("34", "충청남도", null, "admin"),
-                new RegionEntity("35", "전라북도", null, "admin"),
-                new RegionEntity("36", "전라남도", null, "admin"),
-                new RegionEntity("37", "경상북도", null, "admin"),
-                new RegionEntity("38", "경상남도", null, "admin"),
-                new RegionEntity("39", "제주특별자치도", null, "admin")
+        // 부모 지역 데이터 생성
+        List<String[]> initialRegionsData = List.of(
+                new String[]{"11", "서울특별시"},
+                new String[]{"21", "부산광역시"},
+                new String[]{"22", "대구광역시"},
+                new String[]{"23", "인천광역시"},
+                new String[]{"24", "광주광역시"},
+                new String[]{"25", "대전광역시"},
+                new String[]{"26", "울산광역시"},
+                new String[]{"29", "세종특별자치시"},
+                new String[]{"31", "경기도"},
+                new String[]{"32", "강원특별자치도"},
+                new String[]{"33", "충청북도"},
+                new String[]{"34", "충청남도"},
+                new String[]{"35", "전라북도"},
+                new String[]{"36", "전라남도"},
+                new String[]{"37", "경상북도"},
+                new String[]{"38", "경상남도"},
+                new String[]{"39", "제주특별자치도"}
         );
 
-        for (RegionEntity region : initialRegions) {
-            if (!regionRepository.existsByNameAndDeletedAtIsNull(region.getName())) {
-                regionRepository.save(region);
+        for (String[] regionData : initialRegionsData) {
+            String code = regionData[0];
+            String name = regionData[1];
+            if (!regionRepository.existsByNameAndDeletedAtIsNull(name)) {
+                regionRepository.save(RegionEntity.createRegionEntity(
+                        code,
+                        name,
+                        "admin"));
             }
         }
 
         // 부모-자식 지역 설정
-        Map<String, List<RegionEntity>> subRegions = Map.of(
+        Map<String, List<String[]>> subRegions = Map.of(
                 "서울특별시", List.of(
-                        new RegionEntity("11010", "종로구", null, "admin"),
-                        new RegionEntity("11020", "중구", null, "admin"),
-                        new RegionEntity("11030", "용산구", null, "admin"),
-                        new RegionEntity("11040", "성동구", null, "admin"),
-                        new RegionEntity("11050", "광진구", null, "admin"),
-                        new RegionEntity("11060", "동대문구", null, "admin"),
-                        new RegionEntity("11070", "중랑구", null, "admin"),
-                        new RegionEntity("11080", "성북구", null, "admin"),
-                        new RegionEntity("11090", "강북구", null, "admin"),
-                        new RegionEntity("11100", "도봉구", null, "admin"),
-                        new RegionEntity("11110", "노원구", null, "admin"),
-                        new RegionEntity("11120", "은평구", null, "admin"),
-                        new RegionEntity("11130", "서대문구", null, "admin"),
-                        new RegionEntity("11140", "마포구", null, "admin"),
-                        new RegionEntity("11150", "양천구", null, "admin"),
-                        new RegionEntity("11160", "강서구", null, "admin"),
-                        new RegionEntity("11170", "구로구", null, "admin"),
-                        new RegionEntity("11180", "금천구", null, "admin"),
-                        new RegionEntity("11190", "영등포구", null, "admin"),
-                        new RegionEntity("11200", "동작구", null, "admin"),
-                        new RegionEntity("11210", "관악구", null, "admin"),
-                        new RegionEntity("11220", "서초구", null, "admin"),
-                        new RegionEntity("11230", "강남구", null, "admin"),
-                        new RegionEntity("11240", "송파구", null, "admin"),
-                        new RegionEntity("11250", "강동구", null, "admin")
+                        new String[]{"11010", "종로구"},
+                        new String[]{"11020", "중구"},
+                        new String[]{"11030", "용산구"},
+                        new String[]{"11040", "성동구"},
+                        new String[]{"11050", "광진구"},
+                        new String[]{"11060", "동대문구"},
+                        new String[]{"11070", "중랑구"},
+                        new String[]{"11080", "성북구"},
+                        new String[]{"11090", "강북구"},
+                        new String[]{"11100", "도봉구"},
+                        new String[]{"11110", "노원구"},
+                        new String[]{"11120", "은평구"},
+                        new String[]{"11130", "서대문구"},
+                        new String[]{"11140", "마포구"},
+                        new String[]{"11150", "양천구"},
+                        new String[]{"11160", "강서구"},
+                        new String[]{"11170", "구로구"},
+                        new String[]{"11180", "금천구"},
+                        new String[]{"11190", "영등포구"},
+                        new String[]{"11200", "동작구"},
+                        new String[]{"11210", "관악구"},
+                        new String[]{"11220", "서초구"},
+                        new String[]{"11230", "강남구"},
+                        new String[]{"11240", "송파구"},
+                        new String[]{"11250", "강동구"}
                 )
         );
 
-        for (Map.Entry<String, List<RegionEntity>> entry : subRegions.entrySet()) {
+        for (Map.Entry<String, List<String[]>> entry : subRegions.entrySet()) {
             Optional<RegionEntity> parentRegionOptional = regionRepository.findByNameAndDeletedAtIsNull(entry.getKey());
             if (parentRegionOptional.isPresent()) {
                 RegionEntity parentRegion = parentRegionOptional.get();
-                for (RegionEntity subRegion : entry.getValue()) {
-                    if (!regionRepository.existsByNameAndDeletedAtIsNull(subRegion.getName())) {
-                        subRegion.setParent(parentRegion); // 부모 지역 설정
-                        regionRepository.save(subRegion); // 자식 지역 저장
+                for (String[] subRegionData : entry.getValue()) {
+                    String code = subRegionData[0];
+                    String name = subRegionData[1];
+                    if (!regionRepository.existsByNameAndDeletedAtIsNull(name)) {
+                        RegionEntity subRegion = RegionEntity.createRegionEntity(
+                                code,
+                                name,
+                                parentRegion,
+                                "admin");
+                        regionRepository.save(subRegion);
                     }
                 }
             }
