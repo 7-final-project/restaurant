46ea33f602dcc928f89378c43e7103b97591dda1
Jun <dudwns1852@naver.com>
2025-01-20 19:47:46 +0900

    [ refactor ] 식당 검색시 운영 요일 정렬 기준 맞게 반환 ( #20 )

--- src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
+++ src/main/java/com/qring/restaurant/application/v1/res/RestaurantGetByIdResDTOV1.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 
 @Getter
@@ -73,9 +74,23 @@ public class RestaurantGetByIdResDTOV1 {
             private LocalTime closedAt;
 
             public static List<OperatingHour> from(List<OperatingHourEntity> operatingHourEntityList) {
+                // 각 요일 정렬 기준을 만듦
+                List<String> dayOfWeekOrder = List.of("월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일");
+
+                // 정렬 후 변환
                 return operatingHourEntityList.stream()
+                        // Comparator.comparingInt를 사용하여 요일 순서대로 정렬
+                        .sorted(Comparator.comparingInt(
+                                entity -> {
+                                    // 현재 요일을 dayOfWeekOrder 리스트에서 검색하여 인덱스를 가져옴
+                                    int index = dayOfWeekOrder.indexOf(entity.getOperationDayOfWeek());
+                                    // dayOfWeekOrder에 없는 요일은 -1을 반환하므로 Integer.MAX_VALUE로 처리하여 가장 뒤로 보냄
+                                    return index == -1 ? Integer.MAX_VALUE : index;
+                                }
+                        ))
+                        // 정렬된 결과를 OperatingHour 객체로 변환
                         .map(OperatingHour::from)
-                        .toList();
+                        .toList(); // 리스트로 변환하여 반환
             }
 
             public static OperatingHour from(OperatingHourEntity operatingHourEntity) {
